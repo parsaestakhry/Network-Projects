fad3a2b0e916d6e1fb01fa95a4ea571cacd6336f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-12-29 19:11:02 +0330

    Refactor server implementation to read keys from a file and select a random key for each connection

--- Project-1/server/Server.py
+++ Project-1/server/Server.py
@@ -1,24 +1,35 @@
 import socket
+import random
+
 ADDR = socket.gethostbyname(socket.gethostname())
 PORT = 8080
+KEYS_FILE =  r'C:\Users\parsa\Desktop\Network-Projects\Project-1\server\keys.txt'
+
+def read_keys(file_path):
+    with open(file_path, 'r') as file:
+        keys = file.readlines()
+    return [key.strip() for key in keys]
+
+def get_random_key(keys):
+    return random.choice(keys)
+
 def start_server(host=ADDR, port=PORT):
-    # making sure we're using tcp and ipv4
+    keys = read_keys(KEYS_FILE)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # binding the socket to the host and port
         s.bind((host, port))
-        # listening for incoming connections
         s.listen()
-        # printing the host and port
         print(f'Server listening on {host}:{port}')
-        # 
         conn, addr = s.accept()
         with conn:
             print(f'Connected by {addr}')
             while True:
                 data = conn.recv(1024)
-                print(f'Received from client: {data.decode()}')
+                decoded = data.decode()
                 if not data:
                     break
+                print(f'Received from client uppercased : {decoded.upper()}')
+                random_key = get_random_key(keys)
+                print(f'Random key selected: {random_key}')
                 conn.sendall(data)
 
 if __name__ == "__main__":
