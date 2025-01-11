c9bf028857795d6ae136e815386a865c80a027d0
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-11 13:11:35 +0330

    Enhance client to measure round-trip time (RTT) for varying message sizes and improve data reception handling

--- Project-3/Client.py
+++ Project-3/Client.py
@@ -1,20 +1,38 @@
 import socket
+import time
+
 ADDR = socket.gethostbyname(socket.gethostname())
 PORT = 8080
-def start_client(host=ADDR, port=PORT):\
-    # defining the client socket
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # connecting to the server socket
-        s.connect((host, port))
-        # printing the connection
-        print(f'Connected to server at {host}:{port}')
-        while True:
-            message = input("Enter message to send (or 'q' to quit): ")
-            if message.lower() == 'q':
-                break
-            s.sendall(message.encode())
-            data = s.recv(1024)
-            print(f'Received from server: {data.decode()}')
+message_sizes = [1024, 2048, 4096, 8192, 16384]
+
+def start_client(host=ADDR, port=PORT):
+    for size in message_sizes:
+        message = b"1" * size
+        total_rtt = 0
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            print(f'Connected to server at {host}:{port}')
+
+            for i in range(1000):
+                start_time = time.time()
+                s.sendall(message)
+                
+                # Receive data in chunks until we get the full message
+                received_data = b""
+                while len(received_data) < size:
+                    chunk = s.recv(size - len(received_data))
+                    if not chunk:
+                        break
+                    received_data += chunk
+                    
+                end_time = time.time()
+                rtt = end_time - start_time
+                total_rtt += rtt
+                print(f"Message size: {size} bytes")
+                print(f"RTT for message {i+1}: {rtt:.6f} seconds")
+        average_rtt = total_rtt / 1000
+        print(f'average RTT {average_rtt}')
 
 if __name__ == "__main__":
     start_client()
