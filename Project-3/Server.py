cc1a0998dd716e2ff8ea3fc90ac74710d8e272b8
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-01-11 13:11:45 +0330

    Refactor server to improve connection handling and increase buffer size for data reception

--- Project-3/Server.py
+++ Project-3/Server.py
@@ -1,25 +1,23 @@
 import socket
+
 ADDR = socket.gethostbyname(socket.gethostname())
 PORT = 8080
+
 def start_server(host=ADDR, port=PORT):
-    # making sure we're using tcp and ipv4
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # binding the socket to the host and port
         s.bind((host, port))
-        # listening for incoming connections
         s.listen()
-        # printing the host and port
         print(f'Server listening on {host}:{port}')
-        # 
-        conn, addr = s.accept()
-        with conn:
-            print(f'Connected by {addr}')
-            while True:
-                data = conn.recv(1024)
-                print(f'Received from client: {data.decode()}')
-                if not data:
-                    break
-                conn.sendall(data)
+        
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f'Connected by {addr}')
+                while True:
+                    data = conn.recv(16384)
+                    if not data:
+                        break
+                    conn.sendall(data)
 
 if __name__ == "__main__":
     start_server()
